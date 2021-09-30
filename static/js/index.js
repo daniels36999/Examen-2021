d1251092b4c6c07c85fc8db4f0c15a90edca7c23
Daniels <90793016+daniels36999@users.noreply.github.com>
2021-09-29 23:06:57 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,69 +1,24 @@
 //https://www.eclipse.org/paho/clients/js/
 //document.getElementById("sensor1").innerHTML="LED1 ENCENDIDO";
-
+var1=" ";
+var2=" ";
 //FUNCION DEL BOTON ENCENDER - 1
 function LED1_On() {
-	message = new Paho.MQTT.Message("LED1_ON");
+	message = new Paho.MQTT.Message("HISTORIAL1");
 	message.destinationName = "dyautibug.fie@unach.edu.ec/test1";
 	client.send(message); 
-	console.log("LED 1 ENCENDIDO");
+	console.log("MUESTRA HISTORIAL - 2");
+	var1="H1";
 }
 //FUNCION DEL BOTON APAGAR - 1
 function LED1_Off(){	
-	message = new Paho.MQTT.Message("LED1_OFF");
+	message = new Paho.MQTT.Message("HISTORIAL2");
 	message.destinationName ="dyautibug.fie@unach.edu.ec/test1";
 	client.send(message);
-	console.log("LED 1 APAGADO");
+	console.log("MUESTRA HISTORIAL - 2");
+	var2="H2";
 }
 
-//FUNCION DEL BOTON ENCENDER - 2
-function LED2_On() {
-	message = new Paho.MQTT.Message("LED2_ON");
-	message.destinationName = "dyautibug.fie@unach.edu.ec/test1";
-	client.send(message); 
-	console.log("LED 2 ENCENDIDO");
-}
-//FUNCION DEL BOTON APAGAR - 2
-function LED2_Off(){	
-	message = new Paho.MQTT.Message("LED2_OFF");
-	message.destinationName ="dyautibug.fie@unach.edu.ec/test1";
-	client.send(message);
-	console.log("LED 2 APAGADO");
-}
-
-//FUNCION DEL BOTON ENCENDER - 3
-function LED3_On() {
-	message = new Paho.MQTT.Message("LED3_ON");
-	message.destinationName = "dyautibug.fie@unach.edu.ec/test1";
-	client.send(message); 
-	console.log("LED 3 ENCENDIDO");
-}
-//FUNCION DEL BOTON APAGAR - 3
-function LED3_Off(){	
-	message = new Paho.MQTT.Message("LED3_OFF");
-	message.destinationName ="dyautibug.fie@unach.edu.ec/test1";
-	client.send(message);
-	console.log("LED 3 APAGADO");
-}
-
-var1=" ";
-//FUNCION DEL BOTON MOSTRAR HISTORIAL
-function Histo1(){
-	message = new Paho.MQTT.Message("MOSTRAR_HISTORIAL");
-	message.destinationName ="dyautibug.fie@unach.edu.ec/test1";
-	client.send(message);
-	console.log("Se Mostrara el Historial");
-	var1="MM";
-}
-
-//FUNCION DEL BOTON OCULTAR HISTORIAL
-function Histo2(){	
-	message = new Paho.MQTT.Message("OCULTAR_HISTORIAL");
-	message.destinationName ="dyautibug.fie@unach.edu.ec/test1";
-	client.send(message);
-	console.log("Se Ocultara el Historial");
-	var1="OO";
-}
 
 // Create a client instance
   //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
@@ -110,21 +65,20 @@ function Histo2(){
 
   // called when a message arrives
   function onMessageArrived(message) {
-    //console.log("onMessageArrived:"+message.payloadString);
-	  //document.getElementById("sensor").innerHTML=message.payloadString;
-	sms=(message.payloadString);
+	//console.log("onMessageArrived:"+message.payloadString);
+	//document.getElementById("sensor").innerHTML=message.payloadString;
 	  
+	sms=(message.payloadString);
 	VectorDatos=sms.split(";")  
 	Npalabras =VectorDatos.length;
-	document.getElementById("sensor1").innerHTML="Temperatura: "+VectorDatos[0];
-	document.getElementById("sensor2").innerHTML="Distancia: "+VectorDatos[1];
-	document.getElementById("sensor3").innerHTML="Tanque: "+VectorDatos[2];
+	document.getElementById("sensor1").innerHTML="Sensor - 1: "+VectorDatos[0];
+	document.getElementById("sensor2").innerHTML="Sensor - 2: "+VectorDatos[1];
 	  
-	  if(Npalabras>=4&&var1=="MM"){
-	  	document.getElementById("historial").innerHTML=VectorDatos[3];
+	  if(Npalabras>=3&&var1=="H1"){
+	  	document.getElementById("historial1").innerHTML=VectorDatos[2];
 	  }
-	  if(var1=="OO"){
-	  	document.getElementById("historial").innerHTML="Dispositivos Embebidos Historial.";
+	  if(Npalabras>=4&&var2=="H2"){
+	  	document.getElementById("historial2").innerHTML=VectorDatos[3];
 	  }
 	
 	  
@@ -151,16 +105,30 @@ function Histo2(){
 //EXPORTAR
 //----------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------
-function Exportar()
+function Exportar1()
 {      
-    var textToWrite = document.getElementById("historial").innerHTML;
+    var textToWrite = document.getElementById("historial1").innerHTML;
     var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
-    var NombreGuardar = "Embebidos - Historial de Sensores.txt";
+    var NombreGuardar = "Historial-S1.txt";
     var downloadLink = document.createElement("a");
-	
     downloadLink.download = NombreGuardar;
     downloadLink.innerHTML = "My Hidden Link";
+    window.URL = window.URL || window.webkitURL;
+    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+}
 
+function Exportar2()
+{      
+    var textToWrite = document.getElementById("historial2").innerHTML;
+    var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+    var NombreGuardar = "Historial-S2.txt";
+    var downloadLink = document.createElement("a");
+    downloadLink.download = NombreGuardar;
+    downloadLink.innerHTML = "My Hidden Link";
     window.URL = window.URL || window.webkitURL;
     downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
     downloadLink.onclick = destroyClickedElement;
